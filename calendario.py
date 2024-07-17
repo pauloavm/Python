0da2119b886f1eae560579430fc874b427e2b6d8
pauloavm <pauloavmunhoz@gmail.com>
2024-07-17 14:03:09 -0300

    Calendário
    Cria calendário com base no ano, inicialmente para exibição no terminal, porém aplicável em muitos códigos.

---
+++ calendario.py
@@ -0,0 +1,8 @@
+from calendar import *
+year = int(input("Informe o Ano: "))
+print(calendar(year, 2, 1, 8, 3))
+
+# 2 = 2 caracteres da semana (Mo, Tu, etc)
+# 1 = 1 linha por semana
+# 8 = 8 linhas por mês
+# 3 = 3 colunas com exibição do calendário
